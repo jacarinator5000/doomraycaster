fceb7248f5e18663f2fd8fbf1578ad4dcbb2b04a
jacarinator5000 <litjay68@gmail.com>
2024-08-19 21:15:53 -0400

    This is the commit message

---
+++ out/build/raycasterpreset/CMakeFiles/doomraycaster.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doomraycaster.dir/doomraycaster.cpp.o"
+  "CMakeFiles/doomraycaster.dir/doomraycaster.cpp.o.d"
+  "libdoomraycaster.a"
+  "libdoomraycaster.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/doomraycaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ out/build/raycasterpreset/CMakeFiles/doomraycaster.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for doomraycaster.
+# This may be replaced when dependencies are built.
